4cdd3b8c1278ac9d7bb24b5c863018dad0dd443e
Guilherme Haddad <guilherme.haddad@hitransportes.com.br>
2023-11-14 08:23:05 -0300

    Inicializando o repositório com arquivos do https://start.spring.io/

---
+++ src/main/java/ifsuldeminas/gestaoMotoristasVeiculos/GestaoMotoristasVeiculosApplication.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.gestaoMotoristasVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoMotoristasVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoMotoristasVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifsuldeminas/gestaoMotoristasVeiculos/GestaoMotoristasVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.gestaoMotoristasVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoMotoristasVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
