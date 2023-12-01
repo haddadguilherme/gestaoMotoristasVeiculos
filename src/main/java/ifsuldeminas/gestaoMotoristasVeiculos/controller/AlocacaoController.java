934cf855c0b49c145f9b3abd6a72691266649168
Guilherme Haddad <guilherme.haddad@hitransportes.com.br>
2023-12-01 14:55:29 -0300

    Nome alterado.

--- src/main/java/ifsuldeminas/gestaoMotoristasVeiculos/controller/AlocacaoController.java
+++ src/main/java/ifsuldeminas/gestaoMotoristasVeiculos/controller/AlocacaoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/allocation")
+@RequestMapping("/alocacao")
 public class AlocacaoController {
 
     private final AlocacaoService alocacaoService;
